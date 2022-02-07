e8ea65bcf612cd0865a758e59049cbd38ef89bb9
2Bleu <83442032+2Bleu@users.noreply.github.com>
2022-02-07 18:20:21 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function(req, res) {
+  res.send('Hello Back End Class!')
+});
+
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+});
+
+
+
+
+
